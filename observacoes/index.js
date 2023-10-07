30fcd47a2ba0973a7f8ba2c5ea1dba5adec3b6e2
Alunos <Alunos@PTALI20221.animaedu.intranet>
2023-10-06 21:34:33 -0300

    Implementando observações

---
+++ observacoes/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const { v4: uuidv4 } = require('uuid');
+const app = express();
+app.use(express.json());
+const observacoesPorId = {};
+
+app.post("/lembretes/:id/observacoes", (req, res) => {
+    const idObs = uuidv4();
+    const observacoesDoLembrete = observacoesPorId[req.params.id] || [];
+    observacoesDoLembrete.push({ id: idObs, texto });
+
+    observacoesPorId[req.params.id] = observacoesDoLembrete;
+    res.status(201).send(observacoesDoLembrete);
+});
+
+app.get("/lembretes/:id/observacoes", (req, res) => {
+    res.send(observacoesPorId)
+});
+
+app.listen(5000, () => {
+    console.log('Lembretes. Porta 5000');
+});
